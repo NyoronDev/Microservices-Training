74f3d9ab294490cb9005d00f1f60332e8ba0d5ea
Juan Serna <j.Serna@safened.com>
2019-11-05 15:08:19 +0100

    Create picture controller

---
+++ ShoesOnContainers/src/Services/ProductCatalogApi/Controllers/PictureController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace ProductCatalogApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PictureController : ControllerBase
+    {
+        private readonly IWebHostEnvironment environment;
+
+        public PictureController(IWebHostEnvironment environment)
+        {
+            this.environment = environment;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetImage(int id)
+        {
+            var webRoot = environment.WebRootPath;
+            var path = Path.Combine($"{webRoot}{Path.DirectorySeparatorChar}images{Path.DirectorySeparatorChar}", $"shoes-{id}.png");
+            var buffer = System.IO.File.ReadAllBytes(path);
+
+            return File(buffer, "image/png");
+        }
+    }
+}
